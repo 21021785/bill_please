d06cc903eb4162f5c5ca77cf4409a405bf507506
21021785 <21021785@myrp.edu.sg>
2022-05-08 23:15:15 +0800

    Edited

--- app/src/main/java/sg/edu/rp/c346/id21021785/billplease/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id21021785/billplease/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.ToggleButton;
@@ -18,12 +19,15 @@ public class MainActivity extends AppCompatActivity {
     ToggleButton gst;
     EditText discount;
     RadioGroup payment;
+    RadioButton cash;
+    RadioButton payNow;
     Button calculate;
     Button reset;
     TextView total;
     TextView gstAmt;
     TextView svcAmt;
     TextView split;
+    TextView payTo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,32 +40,38 @@ public class MainActivity extends AppCompatActivity {
         gst = findViewById(R.id.gst);
         discount = findViewById(R.id.discountNo);
         payment = findViewById(R.id.paymentMethod);
+        cash = findViewById(R.id.cash);
+        payNow = findViewById(R.id.payNow);
         calculate = findViewById(R.id.calculate);
         reset = findViewById(R.id.reset);
         total = findViewById(R.id.total);
         gstAmt = findViewById(R.id.gstView);
         svcAmt = findViewById(R.id.svcView);
         split = findViewById(R.id.perPerson);
+        payTo = findViewById(R.id.payTo);
 
 
+
+        svc.setTag(0.0);
         svc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (svc.isChecked()) {
-                    double newSvc = 0.1;
+                    svc.setTag(0.1);
                 } else {
-                    double newSvc = 0.0;
+                    svc.setTag(0);
                 }
             }
         });
 
+        gst.setTag(0.07);
         gst.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (gst.isChecked()) {
-                    double newGst = 0.07;
+                    gst.setTag(0.07);
                 } else {
-                    double newGst = 0.0;
+                    gst.setTag(0);
                 }
             }
         });
@@ -72,25 +82,44 @@ public class MainActivity extends AppCompatActivity {
                 double newAmount = Double.parseDouble(amount.getText().toString());
                 int newPax = Integer.parseInt(pax.getText().toString());
                 double newDiscount = Double.parseDouble(discount.getText().toString());
-                double newGst = 0.0;
-                double newSvc = 0.0;
+                double newTotal = 0.0;
+                double newSvc = Double.parseDouble(svc.getTag().toString());
+                System.out.println(gst.getTag());
+                double newGst = Double.parseDouble(gst.getTag().toString());
+                int checkedRadioId = payment.getCheckedRadioButtonId();
 
-                if (svc.isChecked()) {
-                    newSvc = 0.1;
+                System.out.println(R.id.payNow);
+
+                if(checkedRadioId == R.id.cash){
+                    payTo.setText("Pay via cash to John");
+                } else if (checkedRadioId == R.id.payNow) {
+                    payTo.setText("Pay via PayNow to John (91234567)");
                 } else {
-                    newSvc = 0.0;
+                    payTo.setText("");
                 }
 
-                if (gst.isChecked()) {
-                    newGst = 0.07;
+                if (newAmount != 0.0 || newPax != 0 ) {
+                    newTotal = ((newAmount + (newGst * newAmount) + (newSvc * newAmount)) * (100 - newDiscount)/100);
                 } else {
-                    newGst = 0.0;
+                    newTotal = 0;
                 }
 
-                if (newAmount > 0 && newPax > 0) {
-                    double newTotal = newAmount * ((100 - newDiscount)/100) + (newGst * newAmount) + (newSvc * newAmount);                }
+                total.setText(String.format("Total: $%.2f", newTotal));
+                gstAmt.setText(String.format("GST: $%.2f", newGst));
+                svcAmt.setText(String.format("Service Charge: $%.2f", newSvc));
+                split.setText(String.format("Each Person Pays: $%.2f", (newAmount/newPax)));
             }
         });
+
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                amount.setText("");
+                pax.setText("");
+                discount.setText("");
+            }
+        });
+
 
 
 
